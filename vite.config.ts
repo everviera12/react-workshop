418bf168c1cfe1888a2ba67ec0255f2c89114ce9
everviera12 <evertigre10@gmail.com>
2025-07-04 22:53:31 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: 'https://everviera12.github.io/react-workshop/',
   plugins: [react(), tailwindcss()],
 })
